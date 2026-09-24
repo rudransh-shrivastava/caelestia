ce23b348e27764befc91cdb6446f1124b35688a4
Rudransh Shrivastava <rudransh.shr@gmail.com>
2026-09-24 16:12:39 +0530

    chore(user): add monitor layout (eDP-1 left, HDMI-A-1 right)

--- user/hypr-user.lua
+++ user/hypr-user.lua
@@ -1,3 +1,18 @@
+-- Monitors: laptop on the left, external HDMI on the right
+hl.monitor({
+    output   = "eDP-1",
+    mode     = "preferred",
+    position = "0x0",
+    scale    = 1,
+})
+
+hl.monitor({
+    output   = "HDMI-A-1",
+    mode     = "1920x1080@100",
+    position = "1920x0",
+    scale    = 1,
+})
+
 hl.config({
     input = {
         kb_options = "caps:backspace",
